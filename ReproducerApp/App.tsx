668005ba7e8ee87887ad4e200ac78e8c259120e0
ankitgupta-unthinkable <103248056+ankitgupta-unthinkable@users.noreply.github.com>
2025-07-11 18:54:16 +0530

    Update App.tsx

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -6,15 +6,58 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Modal,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Image,
+  Button,
+  Pressable,
+} from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+  const [x,setX] = useState(false);
+
+  const renderLoader = () => {
+    return (
+      <Modal
+        transparent={true}
+        animationType="none"
+        visible={x}
+        onRequestClose={() => {}}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(255, 255, 255, 0.6)',
+          }}
+        >
+          <Image
+            source={require("./img.gif")} // Update the path to your GIF
+            style={{
+              height: 450,
+              width: 450,
+            }}
+            resizeMode="contain"
+          />
+        </View>
+      </Modal>
+    );
+  };
 
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={{height: 100}}/>
+      <Pressable onPress={() => setX(true)} style={{height: 60, width: 100, backgroundColor: 'yellow'}}>Press Me</Pressable>
       <NewAppScreen templateFileName="App.tsx" />
+      {renderLoader()}
     </View>
   );
 }
